1b5930ad85b0d416a722c6595358cefbc69d380d
MaryanQ <maryansalad7@outlook.dk>
2023-05-20 01:49:10 +0200

    kk

--- app.js
+++ app.js
@@ -113,11 +113,15 @@ async function updatePostsGrid() {
 }
 
 // Get all posts - HTTP Method: GET
+function prepareData(data) {
+  return Object.values(data);
+}
+
 async function getPosts() {
-  const response = await fetch(`${endpoint}/posts.json`); // fetch request, (GET)
-  const data = await response.json(); // parse JSON to JavaScript
-  const posts = prepareData(data); // convert object of object to array of objects
-  return posts; // return posts
+  const response = await fetch(`${endpoint}/posts.json`);
+  const data = await response.json();
+  const posts = prepareData(data);
+  return posts;
 }
 
 function showPosts(listOfPosts) {
@@ -128,147 +132,32 @@ function showPosts(listOfPosts) {
   }
 }
 
-function showPost(postObject) {
-  const html = /*html*/ `
-        <article class="grid-item">
-            <img src="${postObject.image}" />
-            <h3>${postObject.title.toUpperCase()}</h3>
-            <p>${postObject.body}</p>
-            <div class="btns">
-                <button class="btn-delete">Delete</button>
-                <button class="btn-update">Update</button>
-            </div>
-        </article>
-    `; // html variable to hold generated html in backtick
-  document.querySelector("#posts").insertAdjacentHTML("beforeend", html); // append html to the DOM - section#posts
+function showPost(post) {
+  console.log(post);
 
-  // add event listeners to .btn-delete and .btn-update
+  const mypost = /* html */ `
+    <article class="grid-item">
+        <h2>${post.title}</h2>
+        <img src="${post.image}" alt=""/>
+        <p>Tryk for mere info: ${post.info}</p>
+      
+    </article>
+  `;
+
+  document.querySelector("#posts").insertAdjacentHTML("beforeend", mypost);
   document
-    .querySelector("#posts article:last-child .btn-delete")
-    .addEventListener("click", deleteClicked);
-  document
-    .querySelector("#posts article:last-child .btn-update")
-    .addEventListener("click", updateClicked);
+    .querySelector("#posts article:last-child")
+    .addEventListener("click", postClicked);
 
-  // called when delete button is clicked
-  function deleteClicked() {
-    console.log("Update button clicked");
+  function postClicked() {
+    console.log("______________________________________");
 
-    document.querySelector("#dialog-delete-post-title").textContent =
-      postObject.title;
-    // set data-id attribute of post you want to delete (... to use when delete)
-    document
-      .querySelector("#form-delete-post")
-      .setAttribute("data-id", postObject.id);
-    // show delete dialog
-    document.querySelector("#dialog-delete-post").showModal();
+    document.querySelector("#detail-image").src = post.image;
+    document.querySelector("#detail-name").textContent = post.title;
+    document.querySelector("#detail-info").textContent = post.body;
+    document.querySelector("#detail-description").textContent =
+      post.description;
+
+    //document.querySelector("#dialog-posts").showModal();
   }
-
-  // called when update button is clicked
-  function updateClicked() {
-    const updateForm = document.querySelector("#form-update-post"); // reference to update form in dialog
-    updateForm.title.value = postObject.title; // set title input in update form from post title
-    updateForm.body.value = postObject.body; // set body input in update form post body
-    updateForm.image.value = postObject.image; // set image input in update form post image
-    updateForm.setAttribute("data-id", postObject.id); // set data-id attribute of post you want to update (... to use when update)
-    document.querySelector("#dialog-update-post").showModal(); // show update modal
-  }
-}
-
-function searchPosts(searchValue) {
-  searchValue = searchValue.toLowerCase();
-
-  const results = posts.filter(checkTitle);
-
-  function checkTitle(post) {
-    const title = post.title.toLowerCase();
-    return title.includes(searchValue);
-  }
-
-  return results;
-}
-
-async function createPost(title, body, image) {
-  event.preventDefault();
-  const newPost = { title, body, image }; // create new post object
-  const json = JSON.stringify(newPost); // convert the JS object to JSON string
-  // POST fetch request with JSON in the body
-  const response = await fetch(`${endpoint}/posts.json`, {
-    method: "POST",
-    body: json,
-  });
-  return response;
-
-  // check if response is ok - if the response is successful
-
-  /*
-  const url = document.querySelector("#image");
-
-  image.addEventListener("input", (event) => {
-    if (url.validity.typeMismatch) {
-      url.setCustomValidity("Please input url image");
-    } else {
-      url.setCustomValidity("");
-    }
-  });*/
-}
-
-// Update an existing post - HTTP Method: DELETE
-async function deletePost(id) {
-  const response = await fetch(`${endpoint}/posts/${id}.json`, {
-    method: "DELETE",
-  });
-  if (response.ok) {
-    console.log("New post succesfully deleted from Firebase 🔥");
-    updatePostsGrid(); // update the post grid to display all posts and the new post
-  }
-}
-
-// Delete an existing post - HTTP Method: PUT
-async function updatePost(id, title, body, image) {
-  const postToUpdate = { title, body, image };
-  const json = JSON.stringify(postToUpdate);
-
-  const response = await fetch(`${endpoint}/posts/${id}.json`, {
-    method: "PUT",
-    body: json,
-  });
-
-  if (response.ok) {
-    console.log("Post succesfully updated in Firebase 🔥");
-    updatePostsGrid(); // update the post grid to display all posts and the new post
-  }
-}
-
-// ============== helper function ============== //
-
-// convert object of objects til an array of objects
-function prepareData(dataObject) {
-  const array = []; // define empty array
-  // loop through every key in dataObject
-  // the value of every key is an object
-  for (const key in dataObject) {
-    const object = dataObject[key]; // define object
-    object.id = key; // add the key in the prop id
-    array.push(object); // add the object to array
-  }
-  return array; // return array back to "the caller"
-}
-
-function compareTitle(post1, post2) {
-  return post1.title.localeCompare(post2.title);
-}
-
-function compareBody(post1, post2) {
-  return post1.body.localeCompare(post2.body);
-}
-
-function showErrorMessage(message) {
-  document.querySelector("#error-message").textContent = message;
-  document.querySelector("#error-message").classList.remove("hide");
-}
-
-function hideErrorMessage() {
-  document.querySelector(".error-message").textContent = "";
-  document.querySelector(".error-message").classList.add("hide");
 }
